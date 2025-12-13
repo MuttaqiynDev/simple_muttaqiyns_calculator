b4aa8ccd3ce70bfbdc7f4878bcc516211e6414f2
Abdulazizxon Saydaliyev <abdulazizxonsaydaliyev@gmail.com>
2025-12-13 22:53:30 +0500

    code changed

--- main.py
+++ main.py
@@ -1,91 +1,59 @@
 import tkinter as tk
 
-calculation = ""
-
-
-def add_to_calculation(symbol):
-    global calculation
-    calculation += str(symbol)
-    text_result.delete(1.0, "end")
-    text_result.insert(1.0, calculation)
-
-
-def evaluate_calculation():
-    global calculation
-    try:
-        calculation = str(eval(calculation))
-        text_result.delete(1.0, "end")
-        text_result.insert(1.0, calculation)
-    except:
-        clear_field()
-        text_result.insert(1.0, "Error")
-
-
-def clear_field():
-    global calculation
-    calculation = ""
-    text_result.delete(1.0, "end")
-
-
 root = tk.Tk()
-root.geometry("300x275")
+root.title('Kalkulyator')
+root.geometry('320x430')
+root.resizable(False, False)
 
-text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
-text_result.grid(columnspan=5)
+equation = tk.StringVar()
 
-btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(
-    1), width=5, font=("Arial", 14))
-btn_1.grid(row=2, column=1)
-btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(
-    2), width=5, font=("Arial", 14))
-btn_2.grid(row=2, column=2)
-btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(
-    3), width=5, font=("Arial", 14))
-btn_3.grid(row=2, column=3)
-btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(
-    4), width=5, font=("Arial", 14))
-btn_4.grid(row=3, column=1)
-btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(
-    5), width=5, font=("Arial", 14))
-btn_5.grid(row=3, column=2)
-btn_6 = tk.Button(root, text="6", command=lambda: add_to_calculation(
-    6), width=5, font=("Arial", 14))
-btn_6.grid(row=3, column=3)
-btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(
-    7), width=5, font=("Arial", 14))
-btn_7.grid(row=4, column=1)
-btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(
-    8), width=5, font=("Arial", 14))
-btn_8.grid(row=4, column=2)
-btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(
-    9), width=5, font=("Arial", 14))
-btn_9.grid(row=4, column=3)
-btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(
-    0), width=5, font=("Arial", 14))
-btn_0.grid(row=5, column=1)
-btn_plus = tk.Button(
-    root, text="+", command=lambda: add_to_calculation("+"), width=5, font=("Arial", 14))
-btn_plus.grid(row=2, column=4)
-btn_min = tk.Button(
-    root, text="-", command=lambda: add_to_calculation("-"), width=5, font=("Arial", 14))
-btn_min.grid(row=3, column=4)
-btn_mul = tk.Button(
-    root, text="*", command=lambda: add_to_calculation("*"), width=5, font=("Arial", 14))
-btn_mul.grid(row=4, column=4)
-btn_div = tk.Button(
-    root, text="/", command=lambda: add_to_calculation("/"), width=5, font=("Arial", 14))
-btn_div.grid(row=5, column=4)
-btn_open = tk.Button(root, text="(", command=lambda: add_to_calculation(
-    "("), width=5, font=("Arial", 14))
-btn_open.grid(row=5, column=2)
-btn_close = tk.Button(root, text=")", command=lambda: add_to_calculation(
-    ")"), width=5, font=("Arial", 14))
-btn_close.grid(row=5, column=3)
-btn_clear = tk.Button(root, text="c", command=clear_field,
-                      width=11, font=("Arial", 14))
-btn_clear.grid(row=6, column=1, columnspan=2)
-btn_eq = tk.Button(root, text="=", command=evaluate_calculation,
-                   width=11, font=("Arial", 14))
-btn_eq.grid(row=6, column=3, columnspan=2)
-root.title("Mutttaqiyndev's Calculator")
+display = tk.Entry(root, textvariable=equation, font=("Arial", 20), bg="#f5f5f5", border=0, justify="right")
+display.pack(fill="both", ipadx=8, ipady=15, pady=(10, 0), padx=10)
+
+def press(num):
+    '''raqamlar bosilganda ularni display qilish uchun function'''
+    current = equation.get()
+    equation.set(current + str(num))
+
+def equalpress(): 
+    '''= bosilganda bu function ishlaydi'''
+    try:
+        natija = str(eval(equation.get()))
+        equation.set(natija)
+    except:
+        equation.set('Xato')
+
+def clear():
+    ''' c bosilganda ko'rinib turgan barcha sonlarni o'chirib yuboradi'''
+    equation.set('')
+
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+# tugmalarni yaratamiz
+tugmalar = [
+    ['7', '8', '9', 'c'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '*'],
+    ['0', '.', '+', '/']
+]
+
+for qator in tugmalar:
+    frame_row = tk.Frame(frame)
+    frame_row.pack(expand=True, fill='both')
+    for tgm_matni in qator:
+        tgm = tk.Button(frame_row,
+            text=tgm_matni,
+            font=("Arial", 18),
+            height=2,
+            width=5,
+            bg="#f0f0f0",
+            activebackground="#dcdcdc",
+            command=lambda x=tgm_matni: clear() if x == 'c' else press(x))
+        tgm.pack(side='left', fill="both", expand=True)
+
+equal_tgm = tk.Button(root, text='=', font=('Arial', 18), bg='#ff5252', fg='white', height=2, width=5, command=equalpress)
+equal_tgm.pack(pady=(5, 10))
+
 root.mainloop()
+
